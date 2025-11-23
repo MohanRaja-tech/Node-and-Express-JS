922b5640c7d4c2b8a08cbec8954b72ff1f03fb13
MohanRaja-tech <mohantwo3@gmail.com>
2025-11-23 21:43:00 +0530

    HTML responses and Status codes with Manual Routings

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 
     const server = http.createServer((req,res) => {
         res.setHeader('Content-Type','text/html');
-        fs.readFile('./docs/index.html',(err,data) => {
+        console.log(req.url);
+        let Path = (path) => {
+            fs.readFile(path,(err,data) => {
         if(err){
             console.log(err.message);
             res.end();
@@ -14,6 +16,26 @@
         }
     
     });
+        }
+        if (req.url == '/'){
+            Path('./docs/index.html');
+        }
+        else if(req.url == '/Home'){
+            res.statusCode = 301;
+            res.setHeader('Location','/');
+            res.end();
+        }
+        else if(req.url == '/About'){
+            Path('./docs/about.html');
+        }
+        else if(req.url == '/Contact'){
+            Path('./docs/contact.html');
+        }
+        else{
+            Path('./docs/notfound.html');
+            res.statusCode = 404;
+        }
+        
     })
     server.listen(5000,'localhost', () => {
         console.log("Server Running on the Port 5000");
