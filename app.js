aab0298c94d75664b742c213664ac94b4adf6cc1
MohanRaja-tech <mohantwo3@gmail.com>
2025-11-26 23:48:50 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');   
 
 app.listen(3000,() => {
     console.log("Server running on the Port 3000");
 });
-
+app.use(morgan('dev'));
+app.use((req,res,next) => {
+    console.log("Middleware 1");
+    next();
+});
+app.use((req,res,next) => {
+    console.log("Middleware 2");
+    next();
+})
 app.get('/',(req,res) => {
     //res.status(200).send('<h1>Hello welcome to the WebPage</h1>');
     res.sendFile('./docs/index.html',{root : __dirname});
@@ -16,5 +25,5 @@ app.get('/main',(req,res) => {
     res.redirect('/about');
 })
 app.use((req,res) => {
-    res.sendFile('./docs/notfound.html',{root : __dirname});
+    res.status(404).sendFile('./docs/notfound.html',{root : __dirname});
 })
